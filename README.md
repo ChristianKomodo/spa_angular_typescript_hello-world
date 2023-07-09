a59209b07a7bf2a9c0f4833612de862f10c5adb5
Christian Knoll <chris@sectorfive.com>
2023-07-09 16:10:18 -0400

    initial clone and config setup

--- README.md
+++ README.md
@@ -43,3 +43,7 @@ npm start
 Visit [`http://localhost:4040/`](http://localhost:4040/) to access the starter application.
 
 In the starter project, all the starter Angular application routes are public. However, you can use Auth0 to get an ID token to hydrate the user profile information present on the `/profile` page with information from a real user. With Auth0, you can also get an access token to make a secure call to an external API to hydrate the messages present in the `/protected` and `/admin` pages.
+
+## Setup Tutorial
+
+Following along with the official tutorial [here](https://developer.auth0.com/resources/guides/spa/angular/basic-authentication).
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment as env } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule.forRoot({
+      ...env.auth0,
+    }),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
